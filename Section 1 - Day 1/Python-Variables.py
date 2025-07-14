ab65f049475309f7e5127e089219dcf36bb88851
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-07-14 22:05:36 +0300

    Add files via upload

---
+++ Section 1 - Day 1/Python-Variables.py
@@ -0,0 +1,36 @@
+# Python Variables:
+
+# name = input("What Is Your Name?")
+# print(name) # Output: The Name You Enter
+
+# Onother Example:
+firstName = "Jacob"
+lastName = "Qumsiyeh"
+print("My Name Is:",firstName,lastName) # Output: My Name Is: Jacob Qumsiyeh
+
+# To Get The Length Of Any String We Use The Method Below
+print(len(firstName)) # Output: 5
+print(len(lastName)) # Output: 8
+
+# Onother Example:
+name = "Jack"
+print(name) # Output: Jack
+name = "Angela"
+print(name) # Output: Angela
+print(len(name)) # Output: 6 Because it took the last print function
+
+
+# Exercise:
+firstName = "Jacob"
+lastName = " Qumsiyeh"
+fullName = firstName + lastName
+print(fullName) # Output: Jacob Qumsiyeh
+print(len(fullName)) # Output: 14 Because of the space between them
+
+# Onother Version Of The Same Exersice:
+firstName = "Jacob"
+lastName = " Qumsiyeh"
+fullName = firstName + lastName
+length = len(fullName)
+print(fullName) # Output: Jacob Qumsiyeh
+print(length) # Output: 14 Because of the space between them
