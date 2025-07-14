7b4f6e9bd8e4a5ae6e46297e9a1f5a246ba1e7f4
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-07-14 21:26:48 +0300

    Add files via upload

---
+++ Section 1 - Day 1/Strings-Manipulation-And-Code-Intelligence.py
@@ -0,0 +1,9 @@
+# 7) Strings Manipulation And Code Intelligence
+# A Method For Saving Time to use in the print method
+# (\n)
+print("Hello World!\nHello World!\nHello Wrold!")
+
+# Concatenation Method // 3 Methods For The Space
+print("Hello" + " Angela") 
+print("Hello " + "Angela")
+print("Hello" + " " + "Angela")
