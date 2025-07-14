ccb1ce495f1be0dd07933825e265543f734ec10b
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-07-14 22:14:36 +0300

    Add files via upload

---
+++ Section 1 - Day 1/variable-naming.py
@@ -0,0 +1,7 @@
+# Variable Naming:
+# it is important to naming the variables to know what it is in the future
+
+username_of_this_person = "Angela" # Ok With This
+length1 = len(username_of_this_person) # it is ok write numbers
+# We Cant Write Like This (user.name = "Angela")
+# Not Ok To Write Variables Like This (1length = len(username_of_this_person))
