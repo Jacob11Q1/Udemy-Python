9797cfae22f3bbec8cf4458287a7a35509815c8f
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-07-14 21:44:40 +0300

    Add files via upload

---
+++ Section 1 - Day 1/The-Python-Input-Function.py
@@ -0,0 +1,6 @@
+# The Python Input Function
+# Instead of using the print function we can use the input function
+
+input("What is you Name?")
+print("Hello " + input("What Is Your Name?: ")) # Output: Hello And the name you enter
+print("Hello " + input("What Is Your Name?: ") + "!") # Output: Hello And the name you enter
